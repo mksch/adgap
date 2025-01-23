a9ad73fb64d9b316f436e3c2f4f4434bb52cf36a
Mary Schultz <mary@mkswebsites.com>
2025-01-23 09:17:45 -0500

    drupal 10.4.1

--- web/themes/custom/adgap/gulpfile.js
+++ web/themes/custom/adgap/gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+//var $    = require('gulp-load-plugins')();
 var uglify = require('gulp-uglify');
-
+var autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass")(require("sass"));
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
@@ -9,26 +10,120 @@ var sassPaths = [
 
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      //outputStyle: 'compressed' // if css compressed **file size**
-      outputStyle: 'expanded' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+  return gulp
+    .src("scss/app.scss")
+    .pipe(
+      sass({
+        includePaths: sassPaths,
+        //outputStyle: 'compressed' // if css compressed **file size**
+        outputStyle: "expanded", // if css compressed **file size**
+        silenceDeprecations: [
+          "abs-percent",
+          "bogus-combinators",
+          "call-string",
+          "color-4-api",
+          "color-functions",
+          "color-module-compat",
+          "css-function-mixin",
+          "duplicate-var-flags",
+          "elseif",
+          "feature-exists",
+          "fs-importer-cwd",
+          "function-units",
+          "global-builtin",
+          "import",
+          "legacy-js-api",
+          "mixed-decls",
+          "moz-document",
+          "new-global",
+          "null-alpha",
+          "relative-canonical",
+          "slash-div",
+          "strict-unary",
+        ]
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions", "ie >= 9"],
+      })
+    )
+    .pipe(gulp.dest("css"));
 });
 gulp.task("editorsass", function () {
   return gulp
     .src("scss/ck-app.scss")
     .pipe(
-      $.sass({
+      sass({
         includePaths: sassPaths,
         outputStyle: "compressed", // if css compressed **file size**
         //outputStyle: 'expanded' // if css compressed **file size**
+        silenceDeprecations: [
+          "abs-percent",
+          "bogus-combinators",
+          "call-string",
+          "color-4-api",
+          "color-functions",
+          "color-module-compat",
+          "css-function-mixin",
+          "duplicate-var-flags",
+          "elseif",
+          "feature-exists",
+          "fs-importer-cwd",
+          "function-units",
+          "global-builtin",
+          "import",
+          "legacy-js-api",
+          "mixed-decls",
+          "moz-document",
+          "new-global",
+          "null-alpha",
+          "relative-canonical",
+          "slash-div",
+          "strict-unary",
+        ],
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions", "ie >= 9"],
+      })
+    )
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task('headersass', function() {
+  return gulp
+    .src("scss/headeronly.scss")
+    .pipe(
+      $.sass({
+        includePaths: sassPaths,
+        //outputStyle: 'compressed' // if css compressed **file size**
+        outputStyle: "expanded", // if css compressed **file size**
+        silenceDeprecations: [
+          "abs-percent",
+          "bogus-combinators",
+          "call-string",
+          "color-4-api",
+          "color-functions",
+          "color-module-compat",
+          "css-function-mixin",
+          "duplicate-var-flags",
+          "elseif",
+          "feature-exists",
+          "fs-importer-cwd",
+          "function-units",
+          "global-builtin",
+          "import",
+          "legacy-js-api",
+          "mixed-decls",
+          "moz-document",
+          "new-global",
+          "null-alpha",
+          "relative-canonical",
+          "slash-div",
+          "strict-unary",
+        ],
       }).on("error", $.sass.logError)
     )
     .pipe(
@@ -39,39 +134,53 @@ gulp.task("editorsass", function () {
     .pipe(gulp.dest("css"));
 });
 
-gulp.task('headersass', function() {
-  return gulp.src('scss/headeronly.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      //outputStyle: 'compressed' // if css compressed **file size**
-      outputStyle: 'expanded' // if css compressed **file size**
-    })
-    .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
-});
-
 gulp.task('skeletonsass', function() {
-  return gulp.src('scss/hiaskeleton.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      //outputStyle: 'compressed' // if css compressed **file size**
-      outputStyle: 'expanded' // if css compressed **file size**
-    })
-    .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+  return gulp
+    .src("scss/hiaskeleton.scss")
+    .pipe(
+      sass({
+        includePaths: sassPaths,
+        //outputStyle: 'compressed' // if css compressed **file size**
+        outputStyle: "expanded", // if css compressed **file size**
+        silenceDeprecations: [
+          "abs-percent",
+          "bogus-combinators",
+          "call-string",
+          "color-4-api",
+          "color-functions",
+          "color-module-compat",
+          "css-function-mixin",
+          "duplicate-var-flags",
+          "elseif",
+          "feature-exists",
+          "fs-importer-cwd",
+          "function-units",
+          "global-builtin",
+          "import",
+          "legacy-js-api",
+          "mixed-decls",
+          "moz-document",
+          "new-global",
+          "null-alpha",
+          "relative-canonical",
+          "slash-div",
+          "strict-unary",
+        ],
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions", "ie >= 9"],
+      })
+    )
+    .pipe(gulp.dest("css"));
 });
 
 // Gulp task to minify JavaScript files
 gulp.task('scripts', function() {
   return gulp.src('lib/main.js')
     // Minify the file
-    .pipe($.uglify())
+    .pipe(uglify())
     // Output
     .pipe(gulp.dest('js/min'))
 });
